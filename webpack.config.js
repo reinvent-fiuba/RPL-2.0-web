51dacb679aeb241eb5553b5ed6e5af24a3fad6eb
Alejandro <levinasale@gmail.com>
2020-07-11 16:45:49 +0100

    Config/env webpack (#49)
    * Configuring .env files to be built according to the webpack mode and with server's env data
    
    * Remove unneded webpack args
    
    * Deleted unused dependencies

--- webpack.config.js
+++ webpack.config.js
@@ -5,58 +5,68 @@ const MonacoWebpackPlugin = require("monaco-editor-webpack-plugin");
 const { BundleAnalyzerPlugin } = require("webpack-bundle-analyzer");
 const CompressionPlugin = require("compression-webpack-plugin");
 
-module.exports = {
-  entry: "./src/index.js",
-  output: {
-    publicPath: "/",
-    path: path.resolve(__dirname, "./dist"),
-    filename: "index_bundle.js",
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
-      },
-      {
-        test: /\.html$/,
-        use: [
-          {
-            loader: "html-loader",
+module.exports = (env, argv) => {
+  // Get the root path (assuming your webpack config is in the root of your project!)
+  const currentPath = path.join(__dirname);
+
+  // Create the fallback path (the production .env)
+  const basePath = `${currentPath}/.env`;
+
+  // We're concatenating the webpack mode name to our filename to specify the correct env file!
+  const envPath = `${basePath}.${argv.mode}`;
+  return {
+    entry: "./src/index.js",
+    output: {
+      publicPath: "/",
+      path: path.resolve(__dirname, "./dist"),
+      filename: "index_bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.(js|jsx)$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
           },
-        ],
-      },
-      {
-        test: /\.css$/,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|jpg|gif|svg)$/i,
-        use: ["file-loader"],
-      },
-      {
-        test: /\.ttf$/,
-        use: ["file-loader"],
-      },
+        },
+        {
+          test: /\.html$/,
+          use: [
+            {
+              loader: "html-loader",
+            },
+          ],
+        },
+        {
+          test: /\.css$/,
+          use: ["style-loader", "css-loader"],
+        },
+        {
+          test: /\.(png|jpg|gif|svg)$/i,
+          use: ["file-loader"],
+        },
+        {
+          test: /\.ttf$/,
+          use: ["file-loader"],
+        },
+      ],
+    },
+    devServer: {
+      historyApiFallback: true,
+    },
+    plugins: [
+      new HtmlWebPackPlugin({
+        template: "./src/index.html",
+        filename: "./index.html",
+      }),
+      new Dotenv({ path: envPath, systemvars: true }),
+      new MonacoWebpackPlugin({
+        // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
+        languages: ["cpp", "javascript", "python", "java"],
+      }),
+      new BundleAnalyzerPlugin({ openAnalyzer: true, analyzerMode: "json" }),
+      new CompressionPlugin(), // https://medium.com/@selvaganesh93/how-to-serve-webpack-gzipped-file-in-production-using-nginx-692eadbb9f1c
     ],
-  },
-  devServer: {
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebPackPlugin({
-      template: "./src/index.html",
-      filename: "./index.html",
-    }),
-    new Dotenv(),
-    new MonacoWebpackPlugin({
-      // available options are documented at https://github.com/Microsoft/monaco-editor-webpack-plugin#options
-      languages: ["cpp", "javascript", "python", "java"],
-    }),
-    new BundleAnalyzerPlugin({ openAnalyzer: true, analyzerMode: "json" }),
-    new CompressionPlugin(), // https://medium.com/@selvaganesh93/how-to-serve-webpack-gzipped-file-in-production-using-nginx-692eadbb9f1c
-  ],
+  };
 };
