5586658c4a413625c0921dd6a5abdf15aae28986
tutecano1995 <tutecano1995@gmail.com>
2020-07-21 01:48:43 -0300

    Add universities and degrees dropdown in signup

--- src/components/SignupPage/SignupForm.js
+++ src/components/SignupPage/SignupForm.js
@@ -1,6 +1,7 @@
 // @flow
 import React from "react";
 import Button from "@material-ui/core/Button";
+import Autocomplete from "@material-ui/lab/Autocomplete";
 import Grid from "@material-ui/core/Grid";
 import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
@@ -44,8 +45,9 @@ type State = {
   name: string,
   surname: string,
   degree: string,
-  university: string,
+  university: any,
   success: boolean,
+  universities: Array<any>,
 };
 
 class Signup extends React.Component<Props, State> {
@@ -57,9 +59,16 @@ class Signup extends React.Component<Props, State> {
     name: "",
     surname: "",
     degree: "",
-    university: "",
+    university: undefined,
     success: false,
+    universities: [],
   };
+
+  componentDidMount() {
+    return authenticationService.getUniversities().then(universities => {
+      this.setState({ universities });
+    });
+  }
 
   handleChange(event, valid) {
     event.persist();
@@ -92,7 +101,7 @@ class Signup extends React.Component<Props, State> {
       error,
     } = this.state;
 
-    if (error.invalidFields.size !== 0) {
+    if (error.invalidFields.size !== 0 || !university || !degree) {
       this.setState(prevState => ({
         error: {
           open: true,
@@ -111,7 +120,7 @@ class Signup extends React.Component<Props, State> {
         name,
         surname,
         degree,
-        university,
+        university: university && university.name,
         studentId,
       })
       .then(() => {
@@ -129,7 +138,7 @@ class Signup extends React.Component<Props, State> {
 
   render() {
     const { classes, history } = this.props;
-    const { error, success } = this.state;
+    const { error, success, universities, university } = this.state;
 
     return (
       <div>
@@ -212,40 +221,24 @@ class Signup extends React.Component<Props, State> {
               this.handleChange(e, validate(e.target.value, /^[0-9a-zA-Z]+$/, "string"))
             }
           />
-          <TextField
+          <Autocomplete
             margin="normal"
-            required
-            fullWidth
-            id="degree"
-            label="Carrera"
-            name="Degree"
-            autoComplete="degree"
-            error={error.invalidFields.has("degree")}
-            helperText={
-              error.invalidFields.has("degree") && "La carrera debe estar formada por letras"
-            }
-            autoFocus
-            onChange={e =>
-              this.handleChange(e, validate(e.target.value, /^[A-zÀ-ÿ\s]+$/, "string"))
-            }
-          />
-          <TextField
-            margin="normal"
-            required
-            fullWidth
+            options={universities}
             id="university"
-            label="Universidad"
-            name="University"
+            name="university"
             autoComplete="university"
-            error={error.invalidFields.has("university")}
-            helperText={
-              error.invalidFields.has("university") &&
-              "La universidad debe estar formada por letras"
-            }
-            autoFocus
-            onChange={e =>
-              this.handleChange(e, validate(e.target.value, /^[A-zÀ-ÿ\s]+$/, "string"))
-            }
+            onChange={(event, newValue) => this.setState({ university: newValue })}
+            getOptionLabel={uni => uni.name}
+            renderInput={params => <TextField {...params} label="Universidad" margin="normal" />}
+          />
+          <Autocomplete
+            margin="normal"
+            options={university ? university.degrees : []}
+            id="degree"
+            name="degree"
+            autoComplete="degree"
+            onChange={(event, newValue) => this.setState({ degree: newValue })}
+            renderInput={params => <TextField {...params} label="Carrera" margin="normal" />}
           />
           <TextField
             margin="normal"
