5024b8e13bc5e7bd522374f01a72d291213d8e82
alejandro <levinasale@gmail.com>
2020-08-02 19:49:30 -0300

    Hacky way to avoid overflow code lines

--- src/components/SolveActivityPage/SolveActivityPage.js
+++ src/components/SolveActivityPage/SolveActivityPage.js
@@ -3,6 +3,7 @@ import React from "react";
 import { withStyles } from "@material-ui/core/styles";
 import SplitPane from "react-split-pane";
 import CircularProgress from "@material-ui/core/CircularProgress";
+import ReactResizeDetector from "react-resize-detector";
 import { withState } from "../../utils/State";
 import TopBar from "../TopBar/TopBar";
 import SideBar from "../SideBar/SideBar";
@@ -52,7 +53,8 @@ const styles = theme => ({
   },
   editor: {
     display: "flex",
-    height: "100%",
+    // height: "100%",
+    height: "85vh",
   },
 });
 
@@ -70,6 +72,7 @@ type State = {
   editorWidth: string,
   submittedActivity: boolean,
   selectedSubmissionId: ?number,
+  editor: any,
   pastSubmissionsPanel: { isOpen: boolean, selectedSubmissionId: ?number },
   finalSolutionId: ?number,
 };
@@ -83,6 +86,7 @@ class SolveActivityPage extends React.Component<Props, State> {
     code: { "main.c": "" },
     submittedActivity: false,
     selectedSubmissionId: null,
+    editor: null,
     pastSubmissionsPanel: { isOpen: false, selectedSubmissionId: null },
     finalSolutionId: null,
   };
@@ -189,6 +193,7 @@ class SolveActivityPage extends React.Component<Props, State> {
       editorWidth,
       error,
       code,
+      editor,
       pastSubmissionsPanel,
       finalSolutionId,
     } = this.state;
@@ -206,8 +211,7 @@ class SolveActivityPage extends React.Component<Props, State> {
             courseId={this.props.match.params.courseId}
             backdropClicked={() => this.setCloseSubmissionsPanel()}
             onSelectSubmission={(submissionId, i) =>
-              this.handleClickOnPastSubmission(submissionId, i)
-            }
+              this.handleClickOnPastSubmission(submissionId, i)}
           />
         )}
 
@@ -253,23 +257,40 @@ class SolveActivityPage extends React.Component<Props, State> {
                 display: "flex",
                 flex: "1 1 0%",
                 height: "100%",
-                position: "fixed",
                 "flex-direction": "row",
+                position: "fixed",
                 overflow: "visible",
                 left: "0px",
                 right: "0px",
               }}
             >
               <div className={classes.editor}>
-                <MultipleTabsEditor
-                  width={editorWidth}
-                  initialCode={code}
-                  language={activity.language.toLowerCase()}
-                  readOnly={false}
-                  forceCanEditFiles={false}
-                  onCodeChange={newCode => this.onCodeChange(newCode)}
-                />
+                <ReactResizeDetector
+                  handleWidth
+                  handleHeight={false}
+                  onResize={() => (editor ? editor.layout : () => {})}
+                >
+                  <MultipleTabsEditor
+                    width={editorWidth}
+                    initialCode={code}
+                    language={activity.language.toLowerCase()}
+                    readOnly={false}
+                    forceCanEditFiles={false}
+                    onCodeChange={newCode => this.onCodeChange(newCode)}
+                    editorDidMount={mountedEditor => {
+                      mountedEditor.changeViewZones(changeAccessor => {
+                        changeAccessor.addZone({
+                          afterLineNumber: 0,
+                          heightInLines: 1,
+                          domNode: document.createElement("span"),
+                        });
+                      });
+                      this.setState({ editor: mountedEditor });
+                    }}
+                  />
+                </ReactResizeDetector>
               </div>
+
               <div>
                 <MarkdownRenderer content={activity.description} />
               </div>
